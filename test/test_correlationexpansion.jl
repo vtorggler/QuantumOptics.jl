90a5b1c82dca336ab9c5f34955d872da9b3f8815
Sebastian Kraemer <basti.kr@gmail.com>
2017-02-10 19:24:05 +0100

    Add more tests for correlationexpansion - seems to work!

--- test/test_correlationexpansion.jl
+++ test/test_correlationexpansion.jl
@@ -19,11 +19,9 @@ mask = correlationexpansion.indices2mask(3, [1,2])
 indices = correlationexpansion.mask2indices(mask)
 @test indices == [1,2]
 
-S1 = correlationexpansion.correlationmasks(4, 1)
 S2 = correlationexpansion.correlationmasks(4, 2)
 S3 = correlationexpansion.correlationmasks(4, 3)
 S4 = correlationexpansion.correlationmasks(4, 4)
-@test S1 == Set([(true, false, false, false), (false, true, false, false), (false, false, true, false), (false, false, false, true)])
 @test S2 == Set([(true, true, false, false), (true, false, true, false), (true, false, false, true),
                  (false, true, true, false), (false, true, false, true), (false, false, true, true)])
 @test S3 == Set([(false, true, true, true), (true, false, true, true), (true, true, false, true), (true, true, true, false)])
@@ -43,6 +41,7 @@ h = 0.5*lazy(h1) ⊗ lazy(h2) ⊗ lazy(h3) ⊗ lazy(h4)
 
 test_op_equal(full(h)*0.3*full(op1_), h*(0.3*op1_))
 test_op_equal(full(op1_)*0.3*full(h), (op1_*0.3)*h)
+test_op_equal(full(h)*full(op1_)*0.3*full(h), h*(op1_*0.3)*h)
 
 # Test ptrace
 test_op_equal(ptrace(full(h)*op1, 1), ptrace(h*op1_, 1))
@@ -60,12 +59,7 @@ test_op_equal(ptrace(full(h)*op1, [1,2,4]), ptrace(h*op1_, [1,2,4]))
 test_op_equal(ptrace(full(h)*op1, [1,3,4]), ptrace(h*op1_, [1,3,4]))
 test_op_equal(ptrace(full(h)*op1, [2,3,4]), ptrace(h*op1_, [2,3,4]))
 
-# Test dmaster
-# b1 = FockBasis(2)
-# b2 = SpinBasis(1//2)
-# b = tensor(b1, b2)
-# S2 = correlationexpansion.correlationmasks(2, 2)
-
+# Compare to standard master time evolution
 psi1 = normalize(Ket(b1, rand(Complex128, length(b1))))
 psi2 = normalize(Ket(b2, rand(Complex128, length(b2))))
 psi3 = normalize(Ket(b3, rand(Complex128, length(b3))))
@@ -79,14 +73,19 @@ rho4 = psi4 ⊗ dagger(psi4)
 rho = rho1 ⊗ rho2 ⊗ rho3 ⊗ rho4
 rho_ = correlationexpansion.approximate(rho, S2 ∪ S3 ∪ S4)
 
+randop(b) = DenseOperator(b, rand(Complex128, length(b), length(b)))
+j1 = lazy(randop(b1)) ⊗ lazy(randop(b2)) ⊗ lazy(randop(b3)) ⊗ lazy(randop(b4))
+j2 = lazy(randop(b1)) ⊗ lazy(randop(b2)) ⊗ lazy(randop(b3)) ⊗ lazy(randop(b4))
+# j1 = LazyTensor(b, [1, 2], [randop(b1), randop(b2)])
+# j2 = LazyTensor(b, [2, 4], [randop(b2), randop(b4)])
+J = LazyTensor[j1, j2]
+v = rand(Float64, length(J))
+Γ = v * transpose(v)
+
 H = LazySum(h, dagger(h))
-T = [0.:0.01:0.1;]
-tout_, rho_t_ = correlationexpansion.master(T, rho_, H, LazyTensor[])
-tout, rho_t = timeevolution.master(T, full(rho_), full(H), [])
-# @time tout_, rho_t_ = correlationexpansion.master(T, rho_, H, LazyTensor[])
-# @time tout_, rho_t_ = correlationexpansion.master(T, rho_, H, LazyTensor[])
-# @time tout, rho_t = timeevolution.master(T, full(rho_), full(H), [])
-# @time tout, rho_t = timeevolution.master(T, full(rho_), full(H), [])
+T = [0.:0.001:0.01;]
+tout_, rho_t_ = correlationexpansion.master(T, rho_, H, J; Gamma=Γ)
+tout, rho_t = timeevolution.master_h(T, full(rho_), full(H), [full(j) for j in J]; Gamma=Γ)
 for i=1:length(rho_t)
     test_op_equal(rho_t[i], rho_t_[i], 1e-5)
 end
