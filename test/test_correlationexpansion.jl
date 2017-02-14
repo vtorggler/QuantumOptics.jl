dca1926526b5dc56168d5474a0193b88e3ab2fdb
Sebastian Kraemer <basti.kr@gmail.com>
2017-02-14 09:25:19 +0100

    Use randop and randdo to create operators.

--- test/test_correlationexpansion.jl
+++ test/test_correlationexpansion.jl
@@ -9,6 +9,9 @@ b3 = NLevelBasis(4)
 b4 = NLevelBasis(2)
 b = tensor(b1, b2, b3, b4)
 
+randdo(b) = (x = normalize(Ket(b, rand(Complex128, length(b)))); x ⊗ dagger(x))
+randop(b) = DenseOperator(b, rand(Complex128, length(b), length(b)))
+
 function test_op_equal(op1, op2, eps=1e-10)
     @test_approx_eq_eps 0. tracedistance_general(full(op1), full(op2)) eps
 end
@@ -33,11 +36,7 @@ op1_ = correlationexpansion.approximate(op1, S2 ∪ S3 ∪ S4)
 test_op_equal(op1, op1_)
 
 # Test multiplication
-h1 = DenseOperator(b1, rand(Complex128, length(b1), length(b1)))
-h2 = DenseOperator(b2, rand(Complex128, length(b2), length(b2)))
-h3 = DenseOperator(b3, rand(Complex128, length(b3), length(b3)))
-h4 = DenseOperator(b4, rand(Complex128, length(b4), length(b4)))
-h = 0.5*lazy(h1) ⊗ lazy(h2) ⊗ lazy(h3) ⊗ lazy(h4)
+h = 0.5*lazy(randop(b1)) ⊗ lazy(randop(b2)) ⊗ lazy(randop(b3)) ⊗ lazy(randop(b4))
 
 test_op_equal(full(h)*0.3*full(op1_), h*(0.3*op1_))
 test_op_equal(full(op1_)*0.3*full(h), (op1_*0.3)*h)
@@ -60,24 +59,11 @@ test_op_equal(ptrace(full(h)*op1, [1,3,4]), ptrace(h*op1_, [1,3,4]))
 test_op_equal(ptrace(full(h)*op1, [2,3,4]), ptrace(h*op1_, [2,3,4]))
 
 # Compare to standard master time evolution
-psi1 = normalize(Ket(b1, rand(Complex128, length(b1))))
-psi2 = normalize(Ket(b2, rand(Complex128, length(b2))))
-psi3 = normalize(Ket(b3, rand(Complex128, length(b3))))
-psi4 = normalize(Ket(b4, rand(Complex128, length(b4))))
-
-rho1 = psi1 ⊗ dagger(psi1)
-rho2 = psi2 ⊗ dagger(psi2)
-rho3 = psi3 ⊗ dagger(psi3)
-rho4 = psi4 ⊗ dagger(psi4)
-
-rho = rho1 ⊗ rho2 ⊗ rho3 ⊗ rho4
+rho = randdo(b1) ⊗ randdo(b2) ⊗ randdo(b3) ⊗ randdo(b4)
 rho_ = correlationexpansion.approximate(rho, S2 ∪ S3 ∪ S4)
 
-randop(b) = DenseOperator(b, rand(Complex128, length(b), length(b)))
 j1 = lazy(randop(b1)) ⊗ lazy(randop(b2)) ⊗ lazy(randop(b3)) ⊗ lazy(randop(b4))
 j2 = lazy(randop(b1)) ⊗ lazy(randop(b2)) ⊗ lazy(randop(b3)) ⊗ lazy(randop(b4))
-# j1 = LazyTensor(b, [1, 2], [randop(b1), randop(b2)])
-# j2 = LazyTensor(b, [2, 4], [randop(b2), randop(b4)])
 J = LazyTensor[j1, j2]
 v = rand(Float64, length(J))
 Γ = v * transpose(v)
