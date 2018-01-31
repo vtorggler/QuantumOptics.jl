fad65dbdaf4dd7fbd205c8e8352a4983e433212a
David Plankensteiner <david.plankensteiner@uibk.ac.at>
2018-01-31 13:34:20 +0100

    Implement stochastic schroedinger for multiple Wiener increments

--- src/stochastic.jl
+++ src/stochastic.jl
@@ -3,8 +3,8 @@ module stochastic
 using ..bases, ..states, ..operators
 using ..operators_dense, ..operators_sparse
 using ..timeevolution
-import ..timeevolution: integrate_stoch, timeevolution_schroedinger.dschroedinger_dynamic
-#using ...operators_lazysum, ...operators_lazytensor, ...operators_lazyproduct
+import ..timeevolution.integrate_stoch
+import ..timeevolution.timeevolution_schroedinger: dschroedinger, dschroedinger_dynamic, check_schroedinger
 import StochasticDiffEq
 
 const DecayRates = Union{Vector{Float64}, Matrix{Float64}, Void}
@@ -32,12 +32,28 @@ function schroedinger_dynamic(tspan, psi0::Ket, fdeterm::Function, fstoch::Funct
                 kwargs...)
     tspan_ = convert(Vector{Float64}, tspan)
     dschroedinger_determ(t::Float64, psi::Ket, dpsi::Ket) = dschroedinger_dynamic(t, psi, fdeterm, dpsi)
-    dschroedinger_stoch(t::Float64, psi::Ket, dpsi::Ket) = dschroedinger_dynamic(t, psi, fstoch, dpsi)
+    dschroedinger_stoch(t::Float64, psi::Ket, dpsi::Ket) = dschroedinger_stochastic(t, psi, fstoch, dpsi)
     x0 = psi0.data
     state = copy(psi0)
     dstate = copy(psi0)
     integrate_stoch(tspan_, dschroedinger_determ, dschroedinger_stoch, x0, state, dstate, fout; kwargs...)
 end
 
+function dschroedinger_stochastic(t::Float64, psi::Ket, f::Function, dpsi::Ket)
+    ops = f(t, psi)
+    if isa(ops, Operator)
+        check_schroedinger(psi, ops)
+        dschroedinger(psi, ops, dpsi)
+    elseif isa(ops, Tuple)
+        out = Array{Ket}(1, length(ops))
+        @inbounds for i=1:length(ops)
+            check_schroedinger(psi, ops[i])
+            out[1, i] = dschroedinger(psi, ops[i], dpsi)
+        end
+        out
+    else
+        ArgumentError("fstoch returns invlaid type $(typeof(ops))!")
+    end
+end
 
 end # module
--- test/test_stochastic.jl
+++ test/test_stochastic.jl
@@ -7,6 +7,7 @@ b_spin = SpinBasis(1//2)
 sz = sigmaz(b_spin)
 sm = sigmam(b_spin)
 sp = sigmap(b_spin)
+zero_op = 0*sz
 
 H = sp + sm
 ψ0 = spindown(b_spin)
@@ -15,21 +16,48 @@ function fdeterm_atom(t, psi)
     H
 end
 function fstoch_atom(t, psi)
-    0*sz
+    zero_op
+end
+function fstoch_2(t, psi)
+    zero_op, zero_op, zero_op
 end
 
 T = [0:0.1:1;]
-tout, ψt = stochastic.schroedinger_dynamic(T, ψ0, fdeterm_atom, fstoch_atom)
-tout, ψt2 = timeevolution.schroedinger_dynamic(T, ψ0, fdeterm_atom)
+tout, ψt1 = stochastic.schroedinger_dynamic(T, ψ0, fdeterm_atom, fstoch_atom)
+tout, ψt2 = stochastic.schroedinger_dynamic(T, ψ0, fdeterm_atom, fstoch_2)
+@test ψt1 == ψt2
 
+tout, ψt_determ = timeevolution.schroedinger_dynamic(T, ψ0, fdeterm_atom)
 for i=1:length(tout)
-    @test norm(ψt[i] - ψt2[i]) < 1e-6
+    @test norm(ψt1[i] - ψt_determ[i]) < 1e-6
 end
 
 
-b_fock = FockBasis(10)
-a = destroy(b_fock)
-ad = create(b_fock)
-
+# Homodyne detection
+# b_fock = FockBasis(10)
+# a = destroy(b_fock)
+# ad = create(b_fock)
+# κ = 1.0
+#
+# # for θ = 0, λ = 1
+# H_determ = 1.0im*κ/2.0*(ad^2 - a^2) - 1.0im*κ*ad*a
+# H_determ2 = 1.0im*2κ*a
+# X = ad + a
+# function fdeterm_hom(t, psi)
+#     H_determ + H_determ2*expect(X, psi)
+# end
+# sq_a = sqrt(2κ)*a
+# function fstoch_hom(t, psi)
+#     sq_a
+# end
+#
+# ψ0 = fockstate(b_fock, 0)
+# T = [0:0.1:10;]
+# tout, ψt = stochastic.schroedinger_dynamic(T, ψ0, fdeterm_hom, fstoch_hom)
+# x = real(expect(X, ψt))
+#
+# using PyPlot
+# plot(x)
+# plot(expect(ad*a, ψt))
 
 end # testset
